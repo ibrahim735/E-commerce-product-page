9d2daa82dd35cc37d0fdc8c35b126dada1e2f0c2
ibrahim735 <adeniranadekunle152@gmail.com>
2025-02-03 00:56:48 +0100

    started logic

---
+++ index.js
@@ -0,0 +1,10 @@
+const productDetails = {
+    productImage_1: "./assets/images/image-product-1.png",
+    productImage_2: "./assets/images/image-product-2.png",
+    productImage_3: "./assets/images/image-product-3.png",
+    productImage_4: "./assets/images/image-product-4.png",
+    productPrice: 125 
+}
+
+const productimg_1 = document.getElementById("img_1")
+productimg_1.src = productDetails.productImage_1
